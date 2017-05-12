40f665d02280fa3211d48ceaed99884f8c220e8a
Álvaro <lvaro>
2017-05-12 18:18:18 +0200

    CADAdquisicion y ENADquisicion completados

---
+++ hadaflix/Biblioteca/biblioteca/CAD/CADAdquisicion.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class CADAdquisicion
+    {
+        private SqlConnection conexion = null;
+
+        //CONSTRUCTOR
+        public CADAdquisicion()
+        {
+            conexion = new SqlConnection();
+            conexion.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\javie\documents\visual studio 2015\Projects\HADAFLIX2\bbdd\bbdd\App_Data\base de datos.mdf;Integrated Security = True; Connect Timeout = 30";
+            conexion.Open();
+        }
+
+        //INSERTAR UNA ADQUISICION
+        public void InsertarAdquisicion(ENAdquisicion adquisicion)
+        {
+            try
+            {
+                string s = adquisicion.Importe.ToString("R");
+                s = s.Replace(",", ".").Replace(".", ".");
+
+                string sentencia = "INSERT INTO adquisicion " +
+                    "(codventa, importe, numfact, cliente, empleado, tipo) VALUES ('" + adquisicion.CodVenta
+                     + "' , " + s + " ,'" + adquisicion.NumFactura + "','" + adquisicion.DniCliente
+                      + "','" + adquisicion.DniEmpleado + "'"+ adquisicion.Tipo +"');";
+                SqlCommand cmd = new SqlCommand(sentencia, conexion);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+
+        //ACTUALIZAR ADQUISICION
+        public void ActualizarAdquisicion(ENAdquisicion adquisicion)
+        {
+            try
+            {
+                string s = adquisicion.Importe.ToString("R");
+                s = s.Replace(",", ".").Replace(".", ".");
+
+                string sentencia = "UPDATE adquisicion SET importe = '" + s + "', numfact = '" + adquisicion.NumFactura
+                     + "',cliente = '" + adquisicion.DniCliente + "', empleado = '" + adquisicion.DniEmpleado 
+                     + "', tipo = '" + adquisicion.Tipo + "' WHERE codventa = " + "'" + adquisicion.CodVenta + "'";
+                SqlCommand cmd = new SqlCommand(sentencia, conexion);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+
+        //ELIMINAR ADQUISICION
+        public void EliminarAdquisicion(ENAdquisicion adquisicion)
+        {
+            try
+            {
+                string sentencia = "DELETE FROM adquisicion WHERE codventa = '" + adquisicion.CodVenta + "';";
+                SqlCommand cmd = new SqlCommand(sentencia, conexion);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+
+        //BUSCAR Y ALMACENAR LISTA DE ADQUISICIONES EXISTENTES
+        public List<ENAdquisicion> BuscarAdquisiciones()
+        {
+            try
+            {
+                List<ENAdquisicion> lAdquisiciones = new List<ENAdquisicion>();
+                string sentencia = "SELECT * FROM adquisicion";
+                ENAdquisicion auxAdquisicion;
+
+                SqlCommand cmd = new SqlCommand(sentencia, conexion);
+                cmd.ExecuteNonQuery();
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+
+
+                if (dr.HasRows)
+                {
+                    do
+                    {
+                        auxAdquisicion = new ENAdquisicion();
+                        auxAdquisicion.CodVenta = dr.GetString(0);
+                        auxAdquisicion.Importe = (float) dr.GetDouble(1);
+                        auxAdquisicion.NumFactura = dr.GetString(2);
+                        auxAdquisicion.DniCliente = dr.GetString(3);
+                        auxAdquisicion.DniEmpleado = dr.GetString(4);
+                        auxAdquisicion.Tipo = dr.GetString(5);
+                        lAdquisiciones.Add(auxAdquisicion);
+                    } while (dr.Read());
+                }
+                return lAdquisiciones;
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+
+        //BUSCAR Y ALMACENAR LISTA DE ALQUILERES EN FUNCION DE UN codVenta
+        public List<ENAdquisicion> BuscarAlquiler(ENAlquiler alq)
+        {
+            try
+            {
+                List<ENAdquisicion> lAdquisiciones = new List<ENAdquisicion>();
+                string sentencia = "SELECT * FROM adquisicion WHERE codventa = '" + alq.CodVenta + "'";
+                ENAdquisicion auxAdquisicion;
+
+                SqlCommand cmd = new SqlCommand(sentencia, conexion);
+                cmd.ExecuteNonQuery();
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+
+
+                if (dr.HasRows)
+                {
+                    do
+                    {
+                        auxAdquisicion = new ENAdquisicion();
+                        auxAdquisicion.CodVenta = dr.GetString(0);
+                        auxAdquisicion.Importe = (float)dr.GetDouble(1);
+                        auxAdquisicion.NumFactura = dr.GetString(2);
+                        auxAdquisicion.DniCliente = dr.GetString(3);
+                        auxAdquisicion.DniEmpleado = dr.GetString(4);
+                        lAdquisiciones.Add(auxAdquisicion);
+                    } while (dr.Read());
+                }
+                return lAdquisiciones;
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+        //BUSCAR Y ALMACENAR LISTA DE ALQUILERES EN FUNCION DE UN codVenta
+        public List<ENAdquisicion> BuscarVenta(ENVenta vent)
+        {
+            try
+            {
+                List<ENAdquisicion> lAdquisiciones = new List<ENAdquisicion>();
+                string sentencia = "SELECT * FROM adquisicion WHERE codventa = '" + vent.CodVenta + "'";
+                ENAdquisicion auxAdquisicion;
+
+                SqlCommand cmd = new SqlCommand(sentencia, conexion);
+                cmd.ExecuteNonQuery();
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+
+
+                if (dr.HasRows)
+                {
+                    do
+                    {
+                        auxAdquisicion = new ENAdquisicion();
+                        auxAdquisicion.CodVenta = dr.GetString(0);
+                        auxAdquisicion.Importe = (float)dr.GetDouble(1);
+                        auxAdquisicion.NumFactura = dr.GetString(2);
+                        auxAdquisicion.DniCliente = dr.GetString(3);
+                        auxAdquisicion.DniEmpleado = dr.GetString(4);
+                        lAdquisiciones.Add(auxAdquisicion);
+                    } while (dr.Read());
+                }
+                return lAdquisiciones;
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+    }
+}
---
+++ hadaflix/Biblioteca/biblioteca/EN/ENAdquisicion.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class ENAdquisicion
+    {
+        protected string _codVenta;
+        protected string _dniCliente;
+        protected string _dniEmpleado;
+        protected float _importe;
+        protected string _numFactura;
+        protected string _tipo;
+
+        #region Constructores
+
+        //DEFECTO
+        public ENAdquisicion()
+        {
+            this._codVenta = "";
+            this._dniCliente = "";
+            this._dniEmpleado = "";
+            this._importe = 0.0f;
+            this._numFactura = "0";
+            this._tipo = "";
+        }
+
+        //PARAMETROS
+        public ENAdquisicion(string codVenta, float importe, string numFactura, string dniCliente, string dniEmpleado, string tipo)
+        {
+            this._codVenta = codVenta;
+            this._dniCliente = dniCliente;
+            this._dniEmpleado = dniEmpleado;
+            this._importe = importe;
+            this._numFactura = numFactura;
+            this._tipo = tipo;
+        }
+        #endregion
+
+        #region Propiedades
+        public string CodVenta
+        {
+            get { return _codVenta; }
+            set { _codVenta = value; }
+        }
+
+        public string DniCliente
+        {
+            get { return _dniCliente; }
+            set { _dniCliente = value; }
+        }
+
+        public string DniEmpleado
+        {
+            get { return _dniEmpleado; }
+            set { _dniEmpleado = value; }
+        }
+
+        public float Importe
+        {
+            get { return _importe; }
+            set { _importe = value; }
+        }
+
+        public string NumFactura
+        {
+            get { return _numFactura; }
+            set { _numFactura = value; }
+        }
+
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+        #endregion
+
+        #region CRUD
+        // CREATE ADQUISICION
+        // ------------------
+        // CREAR/INSERTAR ADQUISICION
+        public void InsertarAdquisicion()
+        {
+            CADAdquisicion cadAdquisicion;
+            cadAdquisicion = new CADAdquisicion();
+            cadAdquisicion.InsertarAdquisicion(this);
+        }
+
+        // READ ADQUISICION
+        // ----------------
+        // BUSCAR/LEER/ALMACENAR LISTA DE ADQUISICIONES
+        public List<ENAdquisicion> BuscarAdquisicion()
+        {
+            List<ENAdquisicion> adquisiciones = new List<ENAdquisicion>();
+            CADAdquisicion c = new CADAdquisicion();
+            adquisiciones = c.BuscarAdquisiciones();
+
+            return adquisiciones;
+        }
+
+        // UPDATE ADQUISICION
+        // ------------------
+        // ACTUALIZAR ADQUISICIONES
+        public void ActualizarAdquisicion()
+        {
+            CADAdquisicion cadAdquisicion;
+            cadAdquisicion = new CADAdquisicion();
+            cadAdquisicion.ActualizarAdquisicion(this);
+        }
+
+        // DELETE ADQUISICION
+        // ------------------
+        // ELIMINAR ADQUISICION
+        public void EliminarAdquisicion()
+        {
+            CADAdquisicion cadAdquisicion;
+            cadAdquisicion = new CADAdquisicion();
+            cadAdquisicion.EliminarAdquisicion(this);
+        }
+        #endregion
+    }
+}
