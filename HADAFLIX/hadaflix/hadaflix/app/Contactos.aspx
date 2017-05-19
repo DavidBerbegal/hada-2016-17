3f9f9f59f8dba7174c6b6d1d79bc7b386c24ae12
Álvaro <lvaro>
2017-05-19 18:52:48 +0200

    aspx de pago i contacto

---
+++ HADAFLIX/hadaflix/hadaflix/app/Contactos.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/app/Site1.Master" AutoEventWireup="true" CodeBehind="Contactos.aspx.cs" Inherits="hadaflix.app.Contactos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <!-- Start Contact Me -->
+     <div id="map"></div>
+    <!-- End Contact Me -->
+    <script>
+
+      // This example displays a marker at the center of Australia.
+      // When the user clicks the marker, an info window opens.
+         
+      function initMap() {
+          var mimapa = { lat: 38.385867, lng: -0.512046 };
+          var CentroMapa = { lat: 38.389, lng: -0.512046 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: CentroMapa,
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.BOTTOM_CENTER
+          },
+          zoomControl: true,
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.LEFT_CENTER
+          },
+          scaleControl: true,
+          streetViewControl: true,
+          streetViewControlOptions: {
+              position: google.maps.ControlPosition.RIGHT_BOTTOM
+          },
+          fullscreenControl: true
+        });
+
+        var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="firstHeading" class="firstHeading">HADAFLIX</h1>' +
+            '<div id="bodyContent">' +
+            '<p>Somo los creadores de <b>HADAFLIX</b>, puede contactar con ' +
+            'nosotros de la siguiente forma:' +
+            '<ol><p>gg52.alu@ua.es</p></ol>' +
+            '<ol><p>jma86@alu.ua.es</p></ol>' +
+            '<ol><p>jrl12@alu.ua.es</p></ol>' +
+            '<ol><p>jrg81@alu.ua.es</p></ol>' +
+            '<ol><p>dbe4@alu.ua.es</p></ol>' +
+            '<p>Asignatura: Edificio 24 - Facultad De Educacion 1, <a href="http://www.dlsi.ua.es/eines/assig.cgi?id=cas&assig=HADA">' +
+            'http://www.dlsi.ua.es/eines/assig.cgi?id=cas&assig=HADA</a> ' +
+            '</p>' +
+            '</div>' +
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+            position: mimapa,
+          map: map,
+          title: 'Edificio 24 - Facultad De Educacion 1'
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+      }
+    </script>
+
+</asp:Content>
---
+++ HADAFLIX/hadaflix/hadaflix/app/Pago.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+using Biblioteca;
+using System.Windows.Forms;
+
+namespace hadaflix.app
+{
+    public partial class Pago : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RealizarPago_Click(object sender, EventArgs e)
+        {
+            string nombre = "";
+            string contraseñaValida = "";
+            string apellido = "";
+            ENCliente cliente = new ENCliente();
+            List<ENCliente> lClientes;
+            ENLineaAdquisicion adq = new ENLineaAdquisicion();
+            float subtotal = adq.subtotalcliente();
+            bool pago = false;
+
+            lClientes = cliente.BuscarCliente();
+
+            for (int i = 0; i < lClientes.Count && pago == false; i++)
+            {
+
+                if (email.Text == lClientes[i].Mail)
+                {
+                    nombre = lClientes[i].Nombre;
+                    contraseñaValida = lClientes[i].Contraseña;
+                    apellido = lClientes[i].Apellidos;
+                    pago = true;
+                }
+
+            }
+
+            if (pago == true)
+            {
+
+
+                MailMessage msj = new MailMessage();
+                SmtpClient cli = new SmtpClient();
+                String correo = email.Text;
+
+
+                msj.From = new MailAddress("hadaflix@gmail.com");
+                msj.To.Add(new MailAddress(correo));
+                msj.Subject = "Bienvenido";
+                string cuerpo = "Hola " + nombre.ToString() + " " + apellido.ToString() + "\nbienvenido a HADAFLIX\n" + "El pago de " + subtotal.ToString("R") + " se ha realizado correctamente.";
+                msj.Body = cuerpo;
+                msj.IsBodyHtml = false;
+
+                cli.Host = "smtp.gmail.com";
+                cli.UseDefaultCredentials = true;
+                cli.Port = 587;
+                cli.Credentials = new NetworkCredential("hadaflix@gmail.com", "hadaflix1234");
+                cli.EnableSsl = true;
+                cli.Send(msj);
+                MessageBox.Show("REVISE SU CORREO. \n       GRACIAS", "HADAFLIX!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                email.Text = "";
+
+                ENLineaAdquisicion linia = new ENLineaAdquisicion();
+
+                linia.EliminarAdquisiciones();
+
+                Response.Redirect("Defecto.aspx");
+            }
+
+            else
+            {
+                MessageBox.Show("No se ha podido realizar el pago correctamente", "Pago", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+                Response.Redirect("Pago.aspx");
+            }
+        }
+    }
+}
